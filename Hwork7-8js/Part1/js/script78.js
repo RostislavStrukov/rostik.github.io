3051a8e846e647ced31fac1588240805b0783b80
rostislav1 <strukovra1@gmail.com>
2016-04-11 20:29:59 +0300

    add 7 - 8

---
+++ Hwork7-8js/Part1/js/script78.js
@@ -0,0 +1,11 @@
+(function($) {
+$(function() {
+
+	$('ul.menu').on('click', 'li:not(.active)', function() {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('div.box-menu').find('div.block-content').removeClass('active').eq($(this).index()).addClass('active');
+	});
+
+});
+})(jQuery);
