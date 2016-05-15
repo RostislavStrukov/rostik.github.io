1d85e62a92d387f4bb55735b54090c77b56bb22b
rostislav1 <strukovra1@gmail.com>
2016-05-15 13:13:04 +0300

    add correct 15-16js

--- Hwork15-16js/js/script.js
+++ Hwork15-16js/js/script.js
@@ -1,3 +1,41 @@
+// создание конструкторов классов 
+function Human() {
+	this.name = 'Ivan',
+	this.age = 30,
+	this.sex = 'male',
+	this.height = 190,
+	this.weight = 90
+}
+
+function Worker() {
+	this.placeWork = 'Company',
+	this.salary = 3000,
+	this.work = function() {
+		alert('He working');
+	}
+}
+
+function Student() {
+	this.placeStudy = 'Universaty',
+	this.studyBursary = 3000,
+	this.study = function() {
+		alert('He studying');
+	}
+}
+
+// создания экземпляров классов
+var newHuman = new Human();
+var newStudent = new Student();
+
+// сохранение методов экземпляров классов 
+Student.prototype = new Worker;
+Human.prototype = new Student;
+
+// вывод данных
+newHuman.study();
+console.log('newWorker.placeStudy', newWorker.placeStudy);
+console.log('newHuman.studyBursary', newHuman.studyBursary);
+
 // Human = {
 // 	name: 'Ivan',
 // 	age: 30,
@@ -25,69 +63,35 @@
 // Human.__proto__ = Student;
 
 // console.log('Human.placeStudy', Human.placeStudy);
+
 // function YahooDemo () {
 // 	console.log('arguments', arguments)
 // }
 
-$.getJSON("http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=PHP&callback=?",
-function(data){
-    var ul = document.createElement("ul");
-    $.each(data.ResultSet.Result, function(i, val){
-           var li = document.createElement("li");
-            var inner = '<a href="'+val.Url+'" title="'+val.Url+'" target="_blank">'+val.Title+"</a>";
-            if (val.Summary != "" && val.Summary != "undefined") {
-                inner += " - "+val.Summary;
-            }
-            li.innerHTML = inner;                               
-            ul.appendChild(li);
-    });
-    $('body').html(ul);
-});
+// $.getJSON("http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=PHP&callback=?",
+// function(data){
+//     var ul = document.createElement("ul");
+//     $.each(data.ResultSet.Result, function(i, val){
+//            var li = document.createElement("li");
+//             var inner = '<a href="'+val.Url+'" title="'+val.Url+'" target="_blank">'+val.Title+"</a>";
+//             if (val.Summary != "" && val.Summary != "undefined") {
+//                 inner += " - "+val.Summary;
+//             }
+//             li.innerHTML = inner;                               
+//             ul.appendChild(li);
+//     });
+//     $('body').html(ul);
+// });
 
 // $(function(){
-	
-
-// $.ajax({
-//             // AJAX-specified URL
-//            url: "http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=PHP&callback=?",
-//            dataType : "jsonp",
-//            // Handle the success event
-//            success: function (data) {
-//                // equal to previuos example
-//                // ...
-//            }
+// 	$.ajax({
+// 	            // AJAX-specified URL
+// 	           url: "http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=PHP&callback=?",
+// 	           dataType : "jsonp",
+// 	           // Handle the success event
+// 	           success: function (data) {
+// 	               // equal to previuos example
+// 	               // ...
+// 	           }
+// 	});
 // });
-
-// });
-function Human() {
-	this.name = 'Ivan',
-	this.age = 30,
-	this.sex = 'male',
-	this.height = 190,
-	this.weight = 90
-}
-
-function Worker() {
-	this.placeWork = 'Company',
-	this.salary = 3000,
-	this.work = function() {
-		alert('He working');
-	}
-}
-
-
-function Student() {
-	this.placeStudy = 'Universaty',
-	this.studyBursary = 3000,
-	this.study = function() {
-		alert('He studying');
-	}
-}
-
-Human.prototype = new Worker;
-Human.prototype = new Student;
-
-var newHuman = new Human();
-
-console.log('newHuman.placeWork', newHuman.placeWork);
-console.log('newHuman.placeStudy', newHuman.placeStudy);
